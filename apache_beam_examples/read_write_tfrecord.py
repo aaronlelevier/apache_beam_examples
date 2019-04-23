af256fb917fbee7af96bdc071d5cdb1bfbfef7a4
aaronlelevier <aaron.lelevier@gmail.com>
2019-04-22 18:03:18 -0700

    maybe_download - function added to fetch MNIST dataset if not present

--- apache_beam_examples/read_write_tfrecord.py
+++ apache_beam_examples/read_write_tfrecord.py
@@ -11,6 +11,7 @@ import tensorflow as tf
 from apache_beam.options.pipeline_options import PipelineOptions
 from logzero import logger
 from tensorflow.python.platform import gfile
+from tensorflow.contrib.learn.python.learn.datasets import mnist
 
 tf.enable_eager_execution()
 
@@ -166,6 +167,7 @@ def write_tfrecords():
     """
     Main write function
     """
+    maybe_download()
     input_dict = {'input-base': '/tmp/data/mnist/val/'}
     with beam.Pipeline(options=PipelineOptions()) as p:
         tf_example = p | "InputSourceToExample" >> _ImageToExample(input_dict)
@@ -176,6 +178,52 @@ def write_tfrecords():
 
         (serialize
          | beam.io.WriteToTFRecord(TFRECORD_OUTFILE, file_name_suffix='.gz'))
+
+
+def maybe_download():
+    """
+    Will download MNIST gzip files and move them to the correct
+    locations if not already present
+    """
+    should_download = True
+    data_dir = '/tmp/data/mnist'
+
+    for x in ['train', 'val']:
+        target_dir = os.path.join(data_dir, x)
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+        else:
+            # dir's exist, so we assume data is already present
+            # and in the correct locations
+            should_download = False
+
+    if not should_download:
+        logger.info('data already present, no need to download')
+        return
+
+    # downloads MNIST datasets
+    data_sets = mnist.read_data_sets(data_dir)
+
+    # move to desired locations and rename
+    def move_to_dest(target_dir, from_file, to_file):
+        try:
+            os.rename(
+                os.path.join(data_dir, from_file),
+                os.path.join(target_dir, to_file)
+            )
+        except OSError:
+            # file already moved
+            pass
+
+    # train
+    train_dir = os.path.join(data_dir, 'train')
+    move_to_dest(train_dir, 'train-images-idx3-ubyte.gz', 'images.gz')
+    move_to_dest(train_dir, 'train-labels-idx1-ubyte.gz', 'labels.gz')
+
+    # test
+    val_dir = os.path.join(data_dir, 'val')
+    move_to_dest(val_dir, 't10k-images-idx3-ubyte.gz', 'images.gz')
+    move_to_dest(val_dir, 't10k-labels-idx1-ubyte.gz', 'labels.gz')
 
 
 def get_raw_dataset(filename):
