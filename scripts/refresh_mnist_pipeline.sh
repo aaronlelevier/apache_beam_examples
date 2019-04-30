73ca70e51f676d7d1ea63737e3338a51ddaa047c
aaronlelevier <aaron.lelevier@gmail.com>
2019-04-30 06:08:35 -0700

    example notebook using TFDV for generating Statistics, Schema, and then checking for Anomalies
    document 'refresh mnist airflow pipeline' script

--- scripts/refresh_mnist_pipeline.sh
+++ scripts/refresh_mnist_pipeline.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# this script refreshes the airflow/tfx local databases by deleting
+# and recreating them
+#
+# when developing, having the same DBs but changing the Airflow Pipeline
+# steps was leading to failures, that then also affected the TFX metadata.db
+
 rm ~/tfx/metadata/mnist/metadata.db
 rm ~/airflow/airflow.db
 
