47908302c5a7af39cabc7fa33210dccbc8e3743e
aaronlelevier <aaron.lelevier@gmail.com>
2019-05-07 18:43:00 -0700

    add step to clear mnist TFX artifacts in  refresh_mnist_pipeline.sh

--- scripts/refresh_mnist_pipeline.sh
+++ scripts/refresh_mnist_pipeline.sh
@@ -6,7 +6,13 @@
 # when developing, having the same DBs but changing the Airflow Pipeline
 # steps was leading to failures, that then also affected the TFX metadata.db
 
+# delete TFX artifacts
+rm -rf ~/tfx/pipelines/mnist
+
+# delete tf-metadata db
 rm ~/tfx/metadata/mnist/metadata.db
+
+# delete airflow db
 rm ~/airflow/airflow.db
 
 airflow initdb
